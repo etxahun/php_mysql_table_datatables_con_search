89beb337c0db27368208fbec5818a71896e33119
Etxahun <etxahun.sanchez@gmail.com>
2016-09-29 17:23:00 +0200

    Initial commit

---
+++ server-response.php
@@ -0,0 +1,187 @@
+<?php
+    /*
+     * Script:    DataTables server-side script for PHP and MySQL
+     * Copyright: 2010 - Allan Jardine, 2012 - Chris Wright
+     * License:   GPL v2 or BSD (3-point)
+     */
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * Easy set variables
+     */
+
+    /* Array of database columns which should be read and sent back to DataTables. Use a space where
+     * you want to insert a non-database field (for example a counter or static image)
+     */
+    $aColumns = array( 'id', 'idproducto', 'codigo', 'nombre', 'marca', 'precio', 'categoria', 'pasillo' );
+
+    /* Indexed column (used for fast and accurate table cardinality) */
+    $sIndexColumn = "id";
+
+    /* DB table to use */
+    $sTable = "producto";
+
+    /* Database connection information */
+    $gaSql['user']       = "root";
+    $gaSql['password']   = "101281";
+    $gaSql['db']         = "supermarket";
+    $gaSql['server']     = "172.17.0.2";
+
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * *
+     * If you just want to use the basic configuration for DataTables with PHP server-side, there is
+     * no need to edit below this line
+     */
+
+    /*
+     * Local functions
+     */
+    function fatal_error ( $sErrorMessage = '' )
+    {
+        header( $_SERVER['SERVER_PROTOCOL'] .' 500 Internal Server Error' );
+        die( $sErrorMessage );
+    }
+
+
+    /*
+     * MySQL connection
+     */
+    $conn = new mysqli( $gaSql['server'], $gaSql['user'], $gaSql['password'], $gaSql['db'] );
+    $acentos = $conn->query("SET NAMES 'utf8'");
+
+    /*
+     * Paging
+     */
+    $sLimit = "";
+    if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' )
+    {
+        $sLimit = "LIMIT ".intval( $_GET['iDisplayStart'] ).", ".
+            intval( $_GET['iDisplayLength'] );
+    }
+
+
+    /*
+     * Ordering
+     */
+    $sOrder = "";
+    if ( isset( $_GET['iSortCol_0'] ) )
+    {
+        $sOrder = "ORDER BY  ";
+        for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ )
+        {
+            if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" )
+            {
+                $sOrder .= $aColumns[ intval( $_GET['iSortCol_'.$i] ) ]."
+                    ".($_GET['sSortDir_'.$i]==='asc' ? 'asc' : 'desc') .", ";
+            }
+        }
+
+        $sOrder = substr_replace( $sOrder, "", -2 );
+        if ( $sOrder == "ORDER BY" )
+        {
+            $sOrder = "";
+        }
+    }
+
+
+    /*
+     * Filtering
+     * NOTE this does not match the built-in DataTables filtering which does it
+     * word by word on any field. It's possible to do here, but concerned about efficiency
+     * on very large tables, and MySQL's regex functionality is very limited
+     */
+    $sWhere = "";
+    if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" )
+    {
+        $sWhere = "WHERE (";
+        for ( $i=0 ; $i<count($aColumns) ; $i++ )
+        {
+            if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" )
+            {
+                $sWhere .= $aColumns[$i]." LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR ";
+            }
+        }
+        $sWhere = substr_replace( $sWhere, "", -3 );
+        $sWhere .= ')';
+    }
+
+    /* Individual column filtering */
+    for ( $i=0 ; $i<count($aColumns) ; $i++ )
+    {
+        if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' )
+        {
+            if ( $sWhere == "" )
+            {
+                $sWhere = "WHERE ";
+            }
+            else
+            {
+                $sWhere .= " AND ";
+            }
+            $sWhere .= $aColumns[$i]." LIKE '%".mysql_real_escape_string($_GET['sSearch_'.$i])."%' ";
+        }
+    }
+
+
+    /*
+     * SQL queries
+     * Get data to display
+     */
+    $sQuery = "
+        SELECT SQL_CALC_FOUND_ROWS ".str_replace(" , ", " ", implode(", ", $aColumns))."
+        FROM   $sTable
+        $sWhere
+        $sOrder
+        $sLimit
+    ";
+    $rResult = $conn->query($sQuery);
+    /* Data set length after filtering */
+    $sQuery = "
+        SELECT FOUND_ROWS()
+    ";
+    $rResultFilterTotal = $conn->query($sQuery);
+    $aResultFilterTotal = $rResultFilterTotal->fetch_assoc();
+    $iFilteredTotal = $aResultFilterTotal[0];
+
+    /* Total data set length */
+    $sQuery = "
+        SELECT COUNT(".$sIndexColumn.")
+        FROM   $sTable
+    ";
+    $rResultTotal = $conn->query($sQuery);
+    $aResultTotal = $rResultTotal->fetch_assoc();
+    $iTotal = $aResultTotal[0];
+
+
+    /*
+     * Output
+     */
+    /*
+	$output = array(
+        "sEcho" => intval($_GET['sEcho']),
+        "iTotalRecords" => $iTotal,
+        "iTotalDisplayRecords" => $iFilteredTotal,
+        "aaData" => array()
+    );
+	*/
+
+    while ( $aRow = $rResult->fetch_assoc() )
+    {
+        $row = array();
+        for ( $i=0 ; $i<count($aColumns) ; $i++ )
+        {
+            if ( $aColumns[$i] == "version" )
+            {
+                /* Special output formatting for 'version' column */
+                $row[] = ($aRow[ $aColumns[$i] ]=="0") ? '-' : $aRow[ $aColumns[$i] ];
+            }
+            else if ( $aColumns[$i] != ' ' )
+            {
+                /* General output */
+                $row[] = $aRow[ $aColumns[$i] ];
+            }
+        }
+        $output['aaData'][] = $row;
+    }
+
+    echo json_encode( $output );
+?>
